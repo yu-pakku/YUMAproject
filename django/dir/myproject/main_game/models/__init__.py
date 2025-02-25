6398ad0f22d903e06b752d2060a40bd683c4ee44
funaryou <funahashi.ryouma@gmail.com>
2025-02-26 08:17:35 +0900

    Revert "Delete kesiwasure"

--- django/dir/myproject/main_game/models/__init__.py
+++ django/dir/myproject/main_game/models/__init__.py
@@ -1,4 +1,3 @@
 from .skills import Skills
 from .afforestation_status import Afforestation_status
 from .tree import Tree
-from .power_up import PowerUp
--- django/dir/myproject/myproject/urls.py
+++ django/dir/myproject/myproject/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('main_game/', include('main_game.urls')),
 ]
