7f5aa5d4ee765d1985d314a54d933485dc6ea406
funa-div <funahashi.ryouma@gmail.com>
2025-03-01 15:25:44 +0900

    自分が作ってしまったデータベースの削除（ゆうまの練習用）

--- django/dir/myproject/main_game/models/__init__.py
+++ django/dir/myproject/main_game/models/__init__.py
@@ -1,4 +1,1 @@
-from .skills import Skills
-from .afforestation_status import Afforestation_status
-from .tree import Tree
 from .power_up import PowerUp
--- django/dir/myproject/main_game/models/tree.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from .afforestation_status import Afforestation_status
-
-
-class Tree(models.Model):
-    grows_area = models.ForeignKey(Afforestation_status, on_delete=models.CASCADE)
-    tree_id = models.AutoField(primary_key=True)
-    tree_age = models.IntegerField(default=0)
-    tree_rank = models.IntegerField(default=1)
